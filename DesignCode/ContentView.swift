242baa01ebc954f8224fb13cddcd6d90f9e8e780
Scott Gardner <scott.gardner@mac.com>
2024-10-08 11:30:52 -0500

    Metal Shaders

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -18,38 +18,51 @@ struct ContentView: View {
     @State var isActive = false
     @State var isDownloading = false
     
+    let startDate = Date.now
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        ZStack {
-            Image(.image1)
-                .resizable()
-                .scaledToFill()
-                .frame(height: isPlaying ? 600 : 300)
-                .frame(width: isPlaying ? 393 : 360)
-                .cornerRadius(isPlaying ? 0 : 20)
-                .offset(y: isPlaying ? -200 : 0)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .strokeBorder(linearGradient)
-                        .opacity(isPlaying ? 0 : 1)
-                )
-                .phaseAnimator([1, 2], trigger: isPlaying) { content, phase in
-                    content.blur(radius: phase == 2 ? 100 : 0)
+        TimelineView(.animation) { context in
+            ZStack {
+                TimelineView(.animation) { context in
+                    Image(.image1)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: isPlaying ? 600 : 300)
+                        .frame(width: isPlaying ? 393 : 360)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 20)
+                                .colorEffect(
+                                    ShaderLibrary.noise(.float(startDate.timeIntervalSinceNow))
+                                )
+                                .blendMode(.softLight)
+                        }
+                        .layerEffect(ShaderLibrary.emboss(.float(10)), maxSampleOffset: .zero)
+                        .layerEffect(ShaderLibrary.pixellate(.float(10)), maxSampleOffset: .zero)
+                        .cornerRadius(isPlaying ? 0 : 20)
+                        .offset(y: isPlaying ? -200 : 0)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .strokeBorder(linearGradient)
+                                .opacity(isPlaying ? 0 : 1)
+                        )
+                        .phaseAnimator([1, 2], trigger: isPlaying) { content, phase in
+                            content.blur(radius: phase == 2 ? 100 : 0)
+                        }
                 }
-            
-            Circle()
-                .fill(.thinMaterial)
-                .frame(width: 100)
-                .overlay(Circle().stroke(.secondary))
-                .overlay(
-                    Image(systemName: "photo")
-                        .font(.largeTitle)
-                )
-                .keyframeAnimator(
-                    initialValue: AnimationValues(),
-                    trigger: isDownloading
-                ) { content, value in
+                
+                Circle()
+                    .fill(.thinMaterial)
+                    .frame(width: 100)
+                    .overlay(Circle().stroke(.secondary))
+                    .overlay(
+                        Image(systemName: "photo")
+                            .font(.largeTitle)
+                    )
+                    .keyframeAnimator(
+                        initialValue: AnimationValues(),
+                        trigger: isDownloading
+                    ) { content, value in
                         content.offset(x: value.position.x, y: value.position.y)
                             .scaleEffect(value.scale)
                     } keyframes: { value in
@@ -68,43 +81,55 @@ struct ContentView: View {
                             CubicKeyframe(1, duration: 0.5)
                         }
                     }
-
-            
-            content
-                .padding(20)
-                .background(.regularMaterial)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .strokeBorder(linearGradient)
-                )
-                .cornerRadius(20)
-                .padding(40)
-                .offset(y: isPlaying ? 220 : 80)
-                .phaseAnimator([1, 1.1], trigger: isPlaying) { content, phase in
-                    content.scaleEffect(phase)
-                } animation: { phase in
-                    switch phase {
-                    case 1: .bouncy
-                    case 1.1: .easeOut
-                    default: .easeInOut
+                
+                
+                content
+                    .padding(20)
+                    .background(.regularMaterial)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .strokeBorder(linearGradient)
+                    )
+                    .cornerRadius(20)
+                    .padding(40)
+                    .offset(y: isPlaying ? 220 : 80)
+                    .phaseAnimator([1, 1.1], trigger: isPlaying) { content, phase in
+                        content.scaleEffect(phase)
+                    } animation: { phase in
+                        switch phase {
+                        case 1: .bouncy
+                        case 1.1: .easeOut
+                        default: .easeInOut
+                        }
                     }
-                }
-            
-            play
-                .frame(width: isPlaying ? 220 : 50)
-                .foregroundStyle(.primary, .white)
-                .font(.largeTitle)
-                .padding(20)
-                .background(.ultraThinMaterial)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .strokeBorder(linearGradient)
-                )
-                .cornerRadius(20)
-                .offset(y: isPlaying ? 40 : -44)
+                
+                play
+                    .frame(width: isPlaying ? 220 : 50)
+                    .foregroundStyle(
+                        ShaderLibrary.angledFill(
+                            .float(10),
+                            .float(10),
+                            .color(.blue)
+                        )
+                    )
+                    .foregroundStyle(.primary, .white)
+                    .font(.largeTitle)
+                    .padding(20)
+                    .background(.ultraThinMaterial)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .strokeBorder(linearGradient)
+                    )
+                    .cornerRadius(20)
+                    .offset(y: isPlaying ? 40 : -44)
+            }
+            .frame(maxWidth: 400)
+            .dynamicTypeSize(.xSmall ... .xLarge)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.blue.opacity(0.001))
+            .distortionEffect(ShaderLibrary.simpleWave(.float(startDate.timeIntervalSinceNow)),
+                              maxSampleOffset: CGSize(width: 100, height: 100))
         }
-        .frame(maxWidth: 400)
-        .dynamicTypeSize(.xSmall ... .xLarge)
     }
     
     var linearGradient: LinearGradient {
